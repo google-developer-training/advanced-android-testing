a471b35abac67f2903748af3af877a6df59d07f4
med25ch <1804233@bdeb.qc.ca>
2023-03-01 11:43:06 -0500

    Testing getActiveAndCompletedStats

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
@@ -22,8 +22,13 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
  * Function that does some trivial computation. Used to showcase unit tests.
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
+
+    if (tasks.isNullOrEmpty()){
+        return StatsResult(0f,0f)
+    }
     val totalTasks = tasks!!.size
     val numberOfActiveTasks = tasks.count { it.isActive }
+
     return StatsResult(
         activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
         completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
