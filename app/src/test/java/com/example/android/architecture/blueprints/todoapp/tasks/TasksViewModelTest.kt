d756a84ada801b1dc502780bc1ee624ff579810f
med25ch <1804233@bdeb.qc.ca>
2023-03-02 11:41:45 -0500

    Writing Assertions for LiveData

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,25 +1,59 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest{
 
+    // Subject under test
+    private lateinit var tasksViewModel: TasksViewModel
+
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
-
-        // Given a fresh TasksViewModel
-        val tasksViewModel =  TasksViewModel(ApplicationProvider.getApplicationContext())
 
         // When adding a new task
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        // Assert that the value is not null
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
+
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+
+        // set filtering to ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the new task event is triggered
+        val value = tasksViewModel.tasksAddViewVisible.getOrAwaitValue()
+
+        // Assert that the value is not null
+        assertThat(value, `is` (true))
 
     }
 }
