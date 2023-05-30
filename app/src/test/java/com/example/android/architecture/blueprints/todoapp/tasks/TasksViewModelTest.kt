33980cac271930115e6a1d23c0fe9347fb7c2cc0
Pankaj Zanzane <pzanzane@gmail.com>
2023-05-30 15:38:01 +0530

    Dependency Injection into ViewModel using ViewModelFactory

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -5,25 +5,34 @@ import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeDefaultTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.IDefaultTasksRepository
 import com.google.common.truth.Truth
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
-@RunWith(AndroidJUnit4::class)
+ 
 internal class TasksViewModelTest {
 
     @get:Rule
     val instantExecutorRule = InstantTaskExecutorRule()
 
+    lateinit var tasksRepository: FakeDefaultTasksRepository
     lateinit var tasksViewModel: TasksViewModel
 
     @Before
     fun setupBefore() {
 
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository = FakeDefaultTasksRepository()
+        tasksRepository.addTask(task1, task2, task3)
+
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     @Test
